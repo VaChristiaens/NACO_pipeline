d41f86879160d9f50fb3b0284cb4b6fe51400a31
Lewis-Picker <61524428+Lewis-Picker@users.noreply.github.com>
2020-07-24 14:26:05 +1000

    Full exapmle

--- run_script.py
+++ run_script.py
@@ -5,19 +5,23 @@ Created on Sun Apr 12 13:11:12 2020
 
 @author: lewis
 """
+#classification stage already completed
 
-import naco_pip
 from naco_pip import input_dataset, raw_dataset
 
-test = input_dataset('/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/raw/',
-                     '/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/classified/')
+clas = input_dataset('/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/raw/',
+                   '/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/classification/')
+clas.bad_columns()
+clas.mk_dico()
+clas.find_sky_in_sci_cube(plot = 'save')
 
-test.bad_columns()
-test.mk_dico()
-test.find_sky_in_sci_cubes()
-
-
-test = raw_dataset('/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/classified/',
+calib = raw_dataset('/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/classification/',
                    '/home/lpic0002/pd87_scratch/products/NACO_archive/5_HD179218/calib/')
 
-
+calib.dark_subtract(debug = False, plot = False)
+calib.flat_field_correction(debug = False, plot = False)
+calib.correct_nan(debug = False, plot = False)
+calib.correct_bad_pixels(debug = False, plot = False)
+calib.first_frames_removal(debug = False, plot = False)
+calib.get_stellar_psf(debug = False, plot = False)
+calib.subtract_sky(debug = False, plot = False)
